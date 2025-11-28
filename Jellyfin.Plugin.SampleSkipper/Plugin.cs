06cb4f67b6d6366427b78d18a2fc9ceb99be75eb
Székely Dániel <80947401+MrHumanRebel@users.noreply.github.com>
2025-11-28 08:56:09 +0100

    Update Plugin.cs

--- Jellyfin.Plugin.SampleSkipper/Plugin.cs
+++ Jellyfin.Plugin.SampleSkipper/Plugin.cs
@@ -7,12 +7,12 @@ namespace Jellyfin.Plugin.SampleSkipper
 {
     public class Plugin : BasePlugin
     {
-        // Egyedi azonosító a plugin számára
+        // Unique GUID for the plugin
         public override Guid Id => Guid.Parse("a4f5d6e7-1b2c-4d3e-5f6g-7h8i9j0k1l2m");
 
         public override string Name => "Sample Skipper";
 
-        public override string Description => "Figyelmen kívül hagyja a 'sample' szót tartalmazó videofájlokat.";
+        public override string Description => "Ignores video files with the word 'sample' in the filename.";
 
         public static Plugin Instance { get; private set; }
 
